d0a4a2968dc5be7940e123b4e64127384c11e917
Hong Tran <jessietran@fb.com>
2021-12-01 13:41:40 -0800

    tried out Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps {
+  text: string;
+  name?: string
+}
+
+interface User {
+  name: string,
+  age: number,
+  country: string,
+  address: {
+    street: string,
+    number: number,
+    zip: string
+  },
+  admin: boolean
+}
+
+export default function App({ text, name= "there" }: AppProps) {
+
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchUser = () => {
+    setUser({
+      name: "Mitchell",
+      age: 23,
+      country: "the Netherlands",
+      address: {
+        street: "Main st.",
+        number: 88,
+        zip: "21345"
+      },
+      admin: false
+    })
+  }
+
   return (
     <div className="App">
       KOSZ
+      <p>{text}</p>
+      <p>Hello {name}!</p>
+      <button onClick={fetchUser}>Fetch user on click</button>
+      {user && <p>{`Welcome ${user.name}`}</p>}
     </div>
   );
 }
 
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App text="cool" name="Hong"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
