f0ffafb0c1848cdf63c92943d851cc2e478b318b
Hong Tran <jessietran@fb.com>
2021-12-03 14:38:11 -0800

    added links

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import '../style/image.css';
 
 export default class Image extends React.Component<any,any> {
     constructor(props:any){
         super(props);
         this.state = {
-
+            showLinks: false
         }
+        this.showInfo = this.showInfo.bind(this)
+    }
+
+    showInfo(){
+        this.setState({ showLinks: true })
+    }
+
+    hideInfo(){
+        this.setState({ showLinks: false })
     }
 
     render(){
 
         const { image } = this.props;
+        const { showLinks } = this.state;
 
         return (
-            <div style={{ width: '240px', margin: '20px', fontFamily: "'Nunito', sans-serif" }}>
-                <img src={image.src} alt={image.title} style={{ borderRadius: '24px', width: '100%'   }}/>
+            <div className="image-container" style={{ width: '240px', margin: '20px', fontFamily: "'Nunito', sans-serif" }}>
+                <div style={{ position: 'relative'}}>
+                    <img 
+                        onMouseOver={() => this.showInfo()} 
+                        onMouseLeave={() => this.hideInfo()}
+                        className="each-image" 
+                        src={image.src} 
+                        alt={image.title} 
+                        style={{ borderRadius: '24px', width: '100%'   }}/>
+                    {showLinks
+                    ? <div style={{ padding: '30px 20px', position: 'absolute', left: 0, bottom: 0, width: '100%', borderRadius: '0 0 24px 24px' }}>
+                        <div style={{ borderRadius: '26px', padding: '6px 10px', backgroundColor: '#F7F7F7', width: '100px', opacity: '0.9'}}>Link</div>
+                      </div>
+                    : null}
+                </div>
                 <p style={{ fontWeight: 'bold', textAlign: 'left' }}>{image.title}</p>
             </div>
         )
--- src/data/images.js
+++ src/data/images.js
@@ -2,146 +2,175 @@ export const images = [
     {
         id: 1,
         src: "https://i.pinimg.com/474x/16/fb/76/16fb76cca872b7b8a372ee86762c2b0b.jpg",
-        title: "しっとり キャロットケーキ"
+        title: "しっとり キャロットケーキ",
+        link: ""
     },
     {
         id: 2,
         src: "https://i.pinimg.com/474x/43/1a/e2/431ae27ed2d528668656b9f9066930f6.jpg",
-        title: "Dakota Johnson Is As Obsessed With These Skater Shoes"
+        title: "Dakota Johnson Is As Obsessed With These Skater Shoes",
+        link: ""
     },
     {
         id: 3,
         src: "https://i.pinimg.com/474x/d9/32/f4/d932f4f6518486c26b9a655c6a01fdb7.jpg",
-        title: "Kinder Cookies"
+        title: "Kinder Cookies",
+        link: ""
     },
     {
         id: 4,
         src: "https://i.pinimg.com/474x/09/b0/49/09b049511a4a062eff23816b12933fda.jpg",
-        title: "Super Soft Condensed Milk Pai Bao"
+        title: "Super Soft Condensed Milk Pai Bao",
+        link: ""
     },
     {
         id: 5,
         src: "https://i.pinimg.com/474x/62/a5/c2/62a5c2e41e8931986dc512f9a7f4be6b.jpg",
-        title: "Alrauna Homestead"
+        title: "Alrauna Homestead",
+        link: ""
     },
     {
         id: 6,
         src: "https://i.pinimg.com/474x/93/e4/d7/93e4d7ea8fad661778b56b6502172513.jpg",
-        title: "Autumn 2012: Celebration"
+        title: "Autumn 2012: Celebration",
+        link: ""
     },
     {
         id: 7,
         src: "https://i.pinimg.com/474x/08/f3/be/08f3be53aaec9a3186aa5349d5f0d2a1.jpg",
-        title: "visualmanifestation"
+        title: "visualmanifestation",
+        link: ""
     },
     {
         id: 8,
         src: "https://i.pinimg.com/474x/a1/10/6e/a1106eb053ebbcb616fd1d9e8a4dcdec.jpg",
-        title: "10 Watercolor Hacks"
+        title: "10 Watercolor Hacks",
+        link: ""
     },
     {
         id: 9,
         src: "https://i.pinimg.com/474x/ce/d7/c1/ced7c175030df1f986cae83c32aa6136.jpg",
-        title: "Dakota Johnson on Instagram"
+        title: "Dakota Johnson on Instagram",
+        link: ""
     },
     {
         id: 10,
         src: "https://i.pinimg.com/474x/12/b2/a3/12b2a3c305862d767fd1fa097fc33828.jpg",
-        title: "Steamed Bao Buns"
+        title: "Steamed Bao Buns",
+        link: ""
     },
     {
         id: 11,
         src: "https://i.pinimg.com/474x/39/cf/08/39cf082762d12da82aa900f8fd250f4a.jpg",
-        title: "The Very Best Looks at the Venice Film Festival This Year"
+        title: "The Very Best Looks at the Venice Film Festival This Year",
+        link: ""
     },
     {
         id: 12,
         src: "https://i.pinimg.com/474x/1d/11/86/1d118611e2a5070791a93e453756e4ba.jpg",
-        title: "5-Ingredient No-Bake Chocolate"
+        title: "5-Ingredient No-Bake Chocolate",
+        link: ""
     },
     {
         id: 13,
         src: "https://i.pinimg.com/474x/df/73/5f/df735ffbf349195d514400442a44f77e.jpg",
-        title: "One Pan Pasta"
+        title: "One Pan Pasta",
+        link: ""
     },
     {
         id: 14,
         src: "https://i.pinimg.com/474x/b4/0e/39/b40e3996c2cc9907a4d6113f3361f073.jpg",
-        title: "Camel Long Sleeves With Elastic Cuffs Round Neckline Shirring Mini Dress"
+        title: "Camel Long Sleeves With Elastic Cuffs Round Neckline Shirring Mini Dress",
+        link: ""
     },
     {
         id: 15,
         src: "https://i.pinimg.com/474x/af/22/a9/af22a90c751e7903d2baf8c0ef9cc23a.jpg",
-        title: "Rust Half Sleeve Round Neck Natural Cotton Plaid Mini Dress With Pocket"
+        title: "Rust Half Sleeve Round Neck Natural Cotton Plaid Mini Dress With Pocket",
+        link: ""
     },
     {
         id: 16,
         src: "https://i.pinimg.com/474x/ae/32/2f/ae322f1f759a3da6ab635d9fdfda4893.jpg",
-        title: ""
+        title: "",
+        link: ""
     },
     {
         id: 17,
         src: "https://i.pinimg.com/474x/48/9b/73/489b73729020cc5b3b249f2922882a30.jpg",
-        title: "la hermana de Lionel Messi"
+        title: "la hermana de Lionel Messi",
+        link: ""
     },
     {
         id: 18,
         src: "https://i.pinimg.com/474x/b6/9f/0f/b69f0f42e5d0243f8c5690d97f941e2b.jpg",
-        title: "Cinnamon twist bread recipe - Chatelaine"
+        title: "Cinnamon twist bread recipe - Chatelaine",
+        link: ""
     },
     {
         id: 19,
         src: "https://i.pinimg.com/474x/ac/b4/ef/acb4ef7a559b9b768dff2c72d109cbdd.jpg",
-        title: "Family Instagram"
+        title: "Family Instagram",
+        link: ""
     },
     {
         id: 20,
         src: "https://i.pinimg.com/474x/ba/cb/45/bacb45bfafd21dcb87688b019fb6e858.jpg",
-        title: "Need A Bigger House?"
+        title: "Need A Bigger House?",
+        link: ""
     },
     {
         id: 21,
         src: "https://i.pinimg.com/474x/49/44/e7/4944e7951762e3b057857b0acd83c6f4.jpg",
-        title: "40 Simple Watercolor Painting Ideas For Beginners To Try"
+        title: "40 Simple Watercolor Painting Ideas For Beginners To Try",
+        link: ""
     },
     {
         id: 22,
         src: "https://i.pinimg.com/474x/a9/e3/e5/a9e3e5b85b304e5fc05e981ba39ad14b.jpg",
-        title: "Pistachio Custard Morning"
+        title: "Pistachio Custard Morning",
+        link: ""
     },
     {
         id: 23,
         src: "https://i.pinimg.com/474x/5b/99/64/5b996450cd7428e06b6c8616c9e6652e.jpg",
-        title: "And now we are eight"
+        title: "And now we are eight",
+        link: ""
     },
     {
         id: 24,
         src: "https://i.pinimg.com/474x/a1/16/8c/a1168c7ac67bfbdb15ce997f33c22977.jpg",
-        title: "Nike Yoga Luxe Leggings"
+        title: "Nike Yoga Luxe Leggings",
+        link: ""
     },
     {
         id: 25,
         src: "https://i.pinimg.com/474x/af/bc/cd/afbccd5abf97710fdf66878196165cfb.jpg",
-        title: "So cute!"
+        title: "So cute!",
+        link: ""
     },
     {
         id: 26,
         src: "https://i.pinimg.com/474x/5c/ad/88/5cad8897ea66e2450a60a036a91bc080.jpg",
-        title: "So ..."
+        title: "So ...",
+        link: ""
     },
     {
         id: 27,
         src: "https://www.pinterest.com/pin/993395630275733120/",
-        title: "Yoga Forever"
+        title: "Yoga Forever",
+        link: ""
     },
     {
         id: 28,
         src: "https://i.pinimg.com/474x/92/60/7f/92607f5101c479066e378ab6b811c6b8.jpg",
-        title: "winter nights"
+        title: "winter nights",
+        link: ""
     },
     {
         id: 29,
         src: "https://i.pinimg.com/474x/61/08/4d/61084dd80927c0737bce475a94b857b6.jpg",
-        title: "#2,898 White Christmas"
+        title: "#2,898 White Christmas",
+        link: ""
     }
 ]
